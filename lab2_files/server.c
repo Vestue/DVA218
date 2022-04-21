027f94c20947b4f98a92d88a047697d0c07c5c11
Meeptard <90312262+Meeptard@users.noreply.github.com>
2022-04-21 12:39:21 +0200

    asdasd

--- lab2_files/server.c
+++ lab2_files/server.c
@@ -106,7 +106,7 @@ int getLength(int* array){
 void broadcast(fd_set activeFdSet, int serverSock){
 	char* broadcastMessage = "A new client has connected!";
 	fd_set writeFdSet = activeFdSet;
-	timeval timeout;
+	struct timeval timeout;
 	timeout->tv_sec = 1;
 	timeout->tv_usec = 500000;
 	for (int i = 0; i < FD_SETSIZE; i++)
