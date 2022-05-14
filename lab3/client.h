bab90f67697af8b0a05df4e0b79a9673d2267ce6
Vestue <61783376+Vestue@users.noreply.github.com>
2022-05-14 16:22:10 +0200

    Remove double macro defines

--- lab3/client.h
+++ lab3/client.h
@@ -3,10 +3,7 @@
 
 #include "wen.c"
 
-/*Defined values*/
-#define SYN 1;
-#define ACK 2;
-#define FIN 4;
+
 // Declarations
 
 int sendMessageToServer(int sock, Datagram toSend, struct sockaddr_in destAddr);
