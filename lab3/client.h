8ae56649b4768d9b20c4a0cb9754f4023e5c0746
Vestue <61783376+Vestue@users.noreply.github.com>
2022-05-22 16:30:19 +0200

    Fix errors

--- lab3/client.h
+++ lab3/client.h
@@ -6,8 +6,8 @@
 /*Defined values here*/
 
 /* Declarations here */
-int writeMessage(ConnectionInfo server, char* message, int* currentSeq);
-int writeMessageGBN(ConnectionInfo server, char* message, int* currentSeq);
-int writeMessageSR(ConnectionInfo server, char* message, int* currentSeq);
+int writeMessage(ConnectionInfo *server, char* message, int* currentSeq);
+int writeMessageGBN(ConnectionInfo *server, char* message, int* currentSeq);
+int writeMessageSR(ConnectionInfo *server, char* message, int* currentSeq);
 
 #endif
